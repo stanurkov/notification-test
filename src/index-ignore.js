8d55ede765e40cf84e8405cb046a32cf9115f210
Stefan Tanurkov <stefan.tanurkov@gmail.com>
2020-04-21 19:28:46 +0300

    first

---
+++ src/index-ignore.js
@@ -0,0 +1,118 @@
+const { app, Notification, BrowserWindow, ipcMain } = require('electron');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+const createWindow = () => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+          nodeIntegration: true,
+        }
+   });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+
+app.on('ready', () => {
+//  app.setAppUserModelId(appId);
+  createWindow();
+});
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+if (false)
+setTimeout(() => {
+  const n = new Notification('Title', {
+    body: 'Lorem Ipsum Dolor Sit Amet'
+  });
+
+  // this is never executed on Windows 10
+  n.on("click", () => {
+    console.log('---==> Notification clicked');
+    mainWindow.webContents.send('post', '---==> Notification clicked');       
+  });
+
+  n.on("close", () => {
+    console.log('X<===--- Notification closed');
+    mainWindow.webContents.send('post', 'X<===--- Notification closed');
+  });
+
+  n.show();
+}, 5000);
+
+
+
+
+const {ToastNotification} = require('electron-windows-notifications')
+
+function showToast() {
+const notification = new ToastNotification({
+    appId: appId,
+    title: 'here it is',
+    subtitle: '1122',
+    group: 'Zip whup',
+    template: `<toast><visual><binding template="ToastText01"><text id="1">%s</text><group><subgroup hint-weight="1"><text>Zipwhip</text></subgroup></group></binding></visual></toast>`,
+    strings: ['Hi!']
+})
+ 
+notification.on('activated', () => {
+    console.log('---==> Notification clicked');
+    mainWindow.webContents.send('post', '---==> Notification clicked');       
+    mainWindow.show();
+});
+ 
+notification.on('closed', () => {
+    console.log('X<===--- Notification closed');
+    mainWindow.webContents.send('post', 'X<===--- Notification closed');
+});
+
+notification.show()
+}
+
+setTimeout(showToast, 5000);
+
+
+ipcMain.on('show', showToast);
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
